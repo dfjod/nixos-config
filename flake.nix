51190c78df685aff861e9fa621322ff27c2bec8f
dfjod <121906325+dfjod@users.noreply.github.com>
2024-11-28 13:53:46 +0000

    Create new configuration base

--- flake.nix
+++ flake.nix
@@ -1,27 +1,16 @@
 {
-	description = "dfjod system configuration";
-
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+		disko.url = "github:nix-community/disko";
+		disko.inputs.nixpkgs.follows = "nixpkgs";
 	};
-
-	outputs = { nixpkgs, home-manager, ... } @ inputs:
-	let
-		system = "x86_64-linux";
-	in {
-		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem { 
-			specialArgs = { inherit inputs; };
-			modules = [ ./nixos/configuration.nix ];
-		};
-
-		homeConfigurations.davis = home-manager.lib.homeManagerConfiguration {
-			pkgs = nixpkgs.legacyPackages.${system};
-			modules = [ ./home-manager/home.nix ];
+	outputs = inputs@{ self, nixpkgs, ...}: {
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				./configuration.nix
+				inputs.disko.nixosModules.disko
+			];
 		};
 	};
 }
